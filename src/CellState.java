6fb43a35f586e62e8131a2be3d1657cae4733857
Darrenfisherlol <darren.nov@gmail.com>
2020-04-05 22:28:51 -0500

    I have finished CellState and need to continue to mend the Todo in order to accommondate the proper finished format

--- src/CellState.java
+++ src/CellState.java
@@ -1,37 +1,44 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public enum CellState {
 
-
-    // OFF ('.')
-    // on ('O')
-    OFF,
-    ON;
+	 OFF('.'),
+	 ON('O');
 
 
 
     private char symbol;
-    private Map<Character, CellState> SYMBOL_TO_STATE = new Map<>();
 
-    private void CellState(char symbol){
+
+    
+    // MAKE SURE HashMap is good
+    private static Map<Character, CellState> SYMBOL_TO_STATE = new HashMap<>();
+    static {
+
+        // OFF ('.')
+        // on ('O')
+
+        SYMBOL_TO_STATE.put('.', OFF);
+        SYMBOL_TO_STATE.put('O', ON);
 
     }
 
-    public CellState getState(char symbol){
 
-    }
-
-    public String toString(){
+    // what else??
+    private CellState(char symbol){
+        this.symbol = symbol;
         
     }
 
+    public static CellState getState(char symbol){
+        return SYMBOL_TO_STATE.get(symbol);
+    }
 
-
-
-
-
-
-
-
+    // Check to make sure it retruns the correct value
+    public String toString(){
+        String ChartoStringReturn = String.valueOf(symbol);
+        return ChartoStringReturn;
+    }
 
 }
--- src/ToDo.md
+++ src/ToDo.md
@@ -1,4 +1,23 @@
-#ToDo
+# Project 2 - Darren Fisher
 
+## Have not filled the code in:
+
+
+
+
+
+
+# Need to edit to make it compile and run:
+
+CellState.Java -[]
+
+
+
+
+
+
+
+# testing that checking that cheking boxes work when displayed
 - [] this is a test to see if this works and does not check off
 - [x] this should be checked off
+
